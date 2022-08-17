3cfb24e75a7938ed70aa563baa3f4d06b15fd792
SamBrianDev <111423117+SamBrianDev@users.noreply.github.com>
2022-08-17 12:49:53 +0200

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,19 +7,21 @@ equal.addEventListener('click', function() {
     /*To check if both inputs are filled in
     Should one or both have no input, the calculations won't happen*/
     document.getElementById("error").innerHTML = "";
-    let input1check;
-    let input2check;
+    let input1Check;
+    let input2Check;
+    let input1NumberCheck;
+    let input2NumberCheck;
     //Input 1 check
     if ( input1.value.length == 0 ) {
         //Input 1 empty
         console.log("Input 1 is empty");
         //Input 1 error
         document.getElementById("error").innerHTML += "Input 1 is leeg<br>";
-        input1check =  false;
+        input1Check =  false;
     }
     else{
         //Input 1 filled in
-        input1check = true;
+        input1Check = true;
     }
     //Input 2 check
     if ( input2.value.length == 0 ) {
@@ -27,35 +29,50 @@ equal.addEventListener('click', function() {
         console.log("Input 2 is empty");
         //Input 2 error
         document.getElementById("error").innerHTML += "Input 2 is leeg<br>";
-        input2check = false;
+        input2Check = false;
     }
     else {
         //Check if imput 2 is 0
         if ( input2.value == 0) {
             //Input 2 is 0
-            input2check = false;
+            input2Check = false;
             document.getElementById("error").innerHTML += "Kan niet verdelen door 0. Probeer opnieuw.<br>";
         }
         else {
             //Input 2 filled in and not 0
-            input2check = true;
+            input2Check = true;
         }
     }
     //Check both inputs
-    if ( input1check == true & input2check == true ) {
+    if ( input1Check == true & input2Check == true ) {
         let input1 = document.getElementById("input1").value;
-        let input2 = document.getElementById("input2").value;
-        
-        if  ( isNaN(input1) && !isNaN(input2)) {
-            window.console.log("Input 1 and input 2 are not numbers.");
-        }
-        else if ( isNaN(input1) ) {
+        //Checks if input 1 is a number
+        if ( isNaN(input1) ) {
+            //If input 1 is a letter
+            input1NumberCheck = false;
             window.console.log("Input 1 is not a number");
-        }
-        else if ( isNaN(input2) ) {
-            window.console.log("Input 2 is not a number");
+            document.getElementById("Error").innerHTML += "Input 1 kan alleen cijfers bevatten";
         }
         else {
+            //If input 1 is a number
+            input1NumberCheck = true;
+            window.console.log("Input 1 is a number");
+        }
+        let input2 = document.getElementById("input2").value;
+        //Checks if input 2 is a number
+        if ( isNaN(input2) ) {
+            //If input 2 is a letter
+            input2NumberCheck = false;
+            indow.console.log("Input 2 is not a number");
+            document.getElementById("Error").innerHTML += "Input 2 kan alleen cijfers bevatten";
+        }
+        else {
+            //If input 2 is a number
+            input2NumberCheck = true;
+            window.console.log("Input 2 is a number");
+        }
+
+        if (input1NumberCheck == true && input2NumberCheck == true ) {
             //Change value inputs into floats
             input1 = parseFloat(input1);
             input2 = parseFloat(input2);
@@ -66,6 +83,9 @@ equal.addEventListener('click', function() {
             vermedigvuldigen(input1, input2);
             verdeel (input1, input2)
             kwadraat (input1, input2);
+        }
+        else {
+            window.console.log("One or both inputs contain a letter, please make the correct adjustments before continuing")
         }
     }
     else {
